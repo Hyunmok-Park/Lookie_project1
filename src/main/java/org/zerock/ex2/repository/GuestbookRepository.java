010543c2948a570d726268db46f922e91f659f08
hyunmok <mokssi2@naver.com>
2022-01-11 09:56:24 +0900

    Add guestbook DTO #8

--- src/main/java/org/zerock/ex2/repository/GuestbookRepository.java
+++ src/main/java/org/zerock/ex2/repository/GuestbookRepository.java
@@ -4,7 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.zerock.ex2.entity.Guestbook;
 
+import java.util.stream.IntStream;
 
 public interface GuestbookRepository extends JpaRepository<Guestbook,
         Long>, QuerydslPredicateExecutor<Guestbook>{
+
+
 }
